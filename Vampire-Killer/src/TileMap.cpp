92224772c2ad0187d1df46c3236fcb670b413320
Ivalpe2 <ivanalvarezperea@gmail.com>
2024-04-11 20:07:49 +0200

    BaseGame

---
+++ Vampire-Killer/src/TileMap.cpp
@@ -0,0 +1,299 @@
+#include "TileMap.h"
+#include "Globals.h"
+#include "ResourceManager.h"
+#include <cstring>
+
+TileMap::TileMap()
+{
+	map = nullptr;
+	width = 0;
+	height = 0;
+	img_tiles = nullptr;
+
+	InitTileDictionary();
+}
+TileMap::~TileMap()
+{
+	if (map != nullptr)
+	{
+		delete[] map;
+		map = nullptr;
+	}
+}
+void TileMap::InitTileDictionary()
+{
+	const int n = TILE_SIZE;
+
+
+	dict_rect[(int)Tile::BLOCK_TREE1] = { 0,  0, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE2] = { n,  0, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE3] = { n * 2,  0, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE4] = { n * 3,  0, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE5] = { n * 4,  0, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE6] = { n * 5,  0, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE7] = { n * 6,  0, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE8] = { n * 7,  0, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE9] = { 0,  n, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE10] = { n,  n, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE11] = { n * 2,  n, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE12] = { n * 3,  n, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE13] = { n * 4,  n, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE14] = { n * 5,  n, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE15] = { n * 6,  n, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE16] = { n * 7,  n, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE17] = { 0,  n * 2, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE18] = { n,  n * 2, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE19] = { n * 2,  n * 2, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE20] = { n * 3,  n * 2, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE21] = { n * 4,  n * 2, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE22] = { n * 5,  n * 2, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE23] = { 0,  n * 3, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE24] = { n,  n * 3, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE25] = { n * 2,  n * 3, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE26] = { n * 3,  n * 3, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE27] = { 0,  n * 4, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE28] = { n,  n * 4, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE29] = { n * 2,  n * 4, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE30] = { n * 3,  n * 4, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE31] = { 0,  n * 5, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE32] = { n,  n * 5, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE33] = { n * 2,  n * 5, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE34] = { n * 3,  n * 5, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE35] = { 0,  n * 6, n, n };
+	dict_rect[(int)Tile::BLOCK_TREE36] = { n,  n * 6, n, n };
+
+	dict_rect[(int)Tile::BLOCK_STATUE1] = { n * 2,  n * 6, n, n };
+	dict_rect[(int)Tile::BLOCK_STATUE2] = { n * 3,  n * 6, n, n };
+	dict_rect[(int)Tile::BLOCK_STATUE3] = { n * 2,  n * 7, n, n };
+	dict_rect[(int)Tile::BLOCK_STATUE4] = { n * 3,  n * 7, n, n };
+	dict_rect[(int)Tile::BLOCK_STATUE5] = { n * 2,  n * 8, n, n };
+	dict_rect[(int)Tile::BLOCK_STATUE6] = { n * 3,  n * 8, n, n };
+
+	dict_rect[(int)Tile::BLOCK_FENCE1] = { 0,  n * 7, n, n };
+	dict_rect[(int)Tile::BLOCK_FENCE2] = { n,  n * 7, n, n };
+	dict_rect[(int)Tile::BLOCK_FENCE3] = { n * 4,  n * 7, n, n };
+	dict_rect[(int)Tile::BLOCK_FENCE4] = { 0,  n * 8, n, n };
+	dict_rect[(int)Tile::BLOCK_FENCE5] = { n,  n * 8, n, n };
+
+	dict_rect[(int)Tile::BLOCK_GROUND] = { 0,  n * 9, n, n };
+	dict_rect[(int)Tile::AIR] = { n,  n * 9, n, n };
+
+}
+AppStatus TileMap::Initialise()
+{
+	ResourceManager& data = ResourceManager::Instance();
+
+	if (data.LoadTexture(Resource::IMG_TILES, "images/tileset.png") != AppStatus::OK)
+	{
+		return AppStatus::ERROR;
+	}
+	img_tiles = data.GetTexture(Resource::IMG_TILES);
+
+	return AppStatus::OK;
+}
+AppStatus TileMap::Load(int data[], int w, int h)
+{
+	size = w * h;
+	width = w;
+	height = h;
+
+	if (map != nullptr)	delete[] map;
+
+	map = new Tile[size];
+	if (map == nullptr)
+	{
+		LOG("Failed to allocate memory for tile map");
+		return AppStatus::ERROR;
+	}
+	memcpy(map, data, size * sizeof(int));
+
+	return AppStatus::OK;
+}
+void TileMap::Update()
+{
+
+}
+Tile TileMap::GetTileIndex(int x, int y) const
+{
+	int idx = x + y * width;
+	if (idx < 0 || idx >= size)
+	{
+		LOG("Error: Index out of bounds. Tile map dimensions: %dx%d. Given index: (%d, %d)", width, height, x, y)
+			return Tile::AIR;
+	}
+	return map[x + y * width];
+}
+bool TileMap::IsTileSolid(Tile tile) const
+{
+	return (Tile::SOLID_FIRST <= tile && tile <= Tile::SOLID_LAST);
+}
+bool TileMap::IsTileLadderTop(Tile tile) const
+{
+	return tile == Tile::LADDER_TOP_L || tile == Tile::LADDER_TOP_R;
+}
+bool TileMap::IsTileLadder(Tile tile) const
+{
+	return tile == Tile::LADDER_L || tile == Tile::LADDER_R;
+}
+bool TileMap::TestCollisionWallLeft(const AABB& box) const
+{
+	return CollisionX(box.pos, box.height);
+}
+bool TileMap::TestCollisionWallRight(const AABB& box) const
+{
+	return CollisionX(box.pos + Point(box.width - 1, 0), box.height);
+}
+bool TileMap::TestCollisionGround(const AABB& box, int* py) const
+{
+	Point p(box.pos.x, *py);	//control point
+	int tile_y;
+
+	if (CollisionY(p, box.width))
+	{
+		tile_y = p.y / TILE_SIZE;
+
+		*py = tile_y * TILE_SIZE - 1;
+		return true;
+	}
+	return false;
+}
+bool TileMap::TestFalling(const AABB& box) const
+{
+	return !CollisionY(box.pos + Point(0, box.height), box.width);
+}
+bool TileMap::CollisionX(const Point& p, int distance) const
+{
+	int x, y, y0, y1;
+
+	//Calculate the tile coordinates and the range of tiles to check for collision
+	x = p.x / TILE_SIZE;
+	y0 = p.y / TILE_SIZE;
+	y1 = (p.y + distance - 1) / TILE_SIZE;
+
+	//Iterate over the tiles within the vertical range
+	for (y = y0; y <= y1; ++y)
+	{
+		//One solid tile is sufficient
+		if (IsTileSolid(GetTileIndex(x, y)))
+			return true;
+	}
+	return false;
+}
+bool TileMap::CollisionY(const Point& p, int distance) const
+{
+	int x, y, x0, x1;
+	Tile tile;
+
+	//Calculate the tile coordinates and the range of tiles to check for collision
+	y = p.y / TILE_SIZE;
+	x0 = p.x / TILE_SIZE;
+	x1 = (p.x + distance - 1) / TILE_SIZE;
+
+	//Iterate over the tiles within the horizontal range
+	for (x = x0; x <= x1; ++x)
+	{
+		tile = GetTileIndex(x, y);
+
+		//One solid or laddertop tile is sufficient
+		if (IsTileSolid(tile) || IsTileLadderTop(tile))
+			return true;
+	}
+	return false;
+}
+bool TileMap::TestOnLadder(const AABB& box, int* px) const
+{
+	int left, right, bottom;
+	int tx1, tx2, ty;
+	Tile tile1, tile2;
+
+	//Control points
+	left = box.pos.x;
+	right = box.pos.x + box.width - 1;
+	bottom = box.pos.y + box.height - 1;
+
+	//Calculate the tile coordinates
+	tx1 = left / TILE_SIZE;
+	tx2 = right / TILE_SIZE;
+	ty = bottom / TILE_SIZE;
+
+	//To be able to climb up or down, both control points must be on ladder
+	tile1 = GetTileIndex(tx1, ty);
+	tile2 = GetTileIndex(tx2, ty);
+	if (IsTileLadder(tile1) && IsTileLadder(tile2))
+	{
+		*px = GetLadderCenterPos(left, bottom) - box.width / 2;
+		return true;
+	}
+	return false;
+}
+bool TileMap::TestOnLadderTop(const AABB& box, int* px) const
+{
+	int left, right, bottom;
+	int tx1, tx2, ty;
+	Tile tile1, tile2;
+
+	//Control points
+	left = box.pos.x;
+	right = box.pos.x + box.width - 1;
+	bottom = box.pos.y + box.height - 1;
+
+	//Calculate the tile coordinates
+	tx1 = left / TILE_SIZE;
+	tx2 = right / TILE_SIZE;
+	ty = bottom / TILE_SIZE;
+
+	//To be able to climb up or down, both control points must be on ladder
+	tile1 = GetTileIndex(tx1, ty);
+	tile2 = GetTileIndex(tx2, ty);
+	if (IsTileLadderTop(tile1) && IsTileLadderTop(tile2))
+	{
+		*px = GetLadderCenterPos(left, bottom) - box.width / 2;
+		return true;
+	}
+	return false;
+}
+int TileMap::GetLadderCenterPos(int pixel_x, int pixel_y) const
+{
+	int tx, ty;
+
+	tx = pixel_x / TILE_SIZE;
+	ty = pixel_y / TILE_SIZE;
+	Tile tile = GetTileIndex(tx, ty);
+
+	if (tile == Tile::LADDER_L || tile == Tile::LADDER_TOP_L)		return tx * TILE_SIZE + TILE_SIZE;
+	else if (tile == Tile::LADDER_R || tile == Tile::LADDER_TOP_R)	return tx * TILE_SIZE;
+	else
+	{
+		LOG("Internal error, tile should be a LADDER, coord: (%d,%d), tile type: %d", pixel_x, pixel_y, (int)tile);
+		return 0;
+	}
+}
+void TileMap::Render()
+{
+	Tile tile;
+	Rectangle rc;
+	Vector2 pos;
+
+	for (int i = 0; i < height; ++i)
+	{
+		for (int j = 0; j < width; ++j)
+		{
+			tile = map[i * width + j];
+			if (tile != Tile::AIR)
+			{
+				pos.x = (float)j * TILE_SIZE;
+				pos.y = (float)i * TILE_SIZE;
+
+				rc = dict_rect[(int)tile];
+				DrawTextureRec(*img_tiles, rc, pos, WHITE);
+			}
+		}
+	}
+}
+void TileMap::Release()
+{
+	ResourceManager& data = ResourceManager::Instance();
+	data.ReleaseTexture(Resource::IMG_TILES);
+
+	dict_rect.clear();
+}
---
+++ Vampire-Killer/src/TileMap.h
@@ -0,0 +1,95 @@
+#pragma once
+#include <raylib.h>
+#include "Sprite.h"
+#include "Point.h"
+#include "AABB.h"
+#include "Globals.h"
+#include <unordered_map>
+
+enum class Tile {
+
+	//  0: empty tile
+	// -1: area covered by entity
+	AIR = 0,
+
+	// 0 < id < 50: trees
+	BLOCK_TREE1 = 1, BLOCK_TREE2, BLOCK_TREE3, BLOCK_TREE4, BLOCK_TREE5, BLOCK_TREE6, BLOCK_TREE7, BLOCK_TREE8,
+	BLOCK_TREE9, BLOCK_TREE10, BLOCK_TREE11, BLOCK_TREE12, BLOCK_TREE13, BLOCK_TREE14, BLOCK_TREE15, BLOCK_TREE16,
+	BLOCK_TREE17, BLOCK_TREE18, BLOCK_TREE19, BLOCK_TREE20, BLOCK_TREE21, BLOCK_TREE22, BLOCK_TREE23, BLOCK_TREE24,
+	BLOCK_TREE25, BLOCK_TREE26, BLOCK_TREE27, BLOCK_TREE28, BLOCK_TREE29, BLOCK_TREE30, BLOCK_TREE31, BLOCK_TREE32, 
+	BLOCK_TREE33, BLOCK_TREE34, BLOCK_TREE35, BLOCK_TREE36,
+
+	//id >= 40: fence
+	BLOCK_STATUE1 = 40, BLOCK_STATUE2, BLOCK_STATUE3, BLOCK_STATUE4, BLOCK_STATUE5, BLOCK_STATUE6, BLOCK_FENCE1,
+	BLOCK_FENCE2, BLOCK_FENCE3, BLOCK_FENCE4, BLOCK_FENCE5,
+
+	//id >= 60: ground
+	BLOCK_GROUND = 60,
+
+	LADDER_L = 70, LADDER_R, LADDER_TOP_L, LADDER_TOP_R,
+
+	// id >= 100: entities' initial locations
+	PLAYER = 100,
+
+	STATIC_FIRST = BLOCK_GROUND,
+	STATIC_LAST = BLOCK_GROUND,
+	SOLID_FIRST = BLOCK_GROUND,
+	SOLID_LAST = BLOCK_GROUND,
+	ENTITY_FIRST = PLAYER,
+	ENTITY_LAST = PLAYER
+};
+
+class TileMap
+{
+public:
+	TileMap();
+	~TileMap();
+
+	AppStatus Initialise();
+	AppStatus Load(int data[], int w, int h);
+	void Update();
+	void Render();
+	void Release();
+
+	//Test for collisions with walls
+	bool TestCollisionWallLeft(const AABB& box) const;
+	bool TestCollisionWallRight(const AABB& box) const;
+	
+	//Test collision with the ground and update 'py' with the maximum y-position to prevent
+	//penetration of the grounded tile, that is, the pixel y-position above the grounded tile.
+	//Grounded tile = solid tile (blocks) or ladder tops.
+	bool TestCollisionGround(const AABB& box, int *py) const;
+	
+	//Test if there is a ground tile one pixel below the given box
+	bool TestFalling(const AABB& box) const;
+	
+	//Test if box is on ladder and update 'px' with the x-center position of the ladder
+	bool TestOnLadder(const AABB& box, int* px) const;
+	
+	//Test if box is on ladder top and update 'px' with the x-center position of the ladder
+	bool TestOnLadderTop(const AABB& box, int* px) const;
+
+private:
+	void InitTileDictionary();
+
+	Tile GetTileIndex(int x, int y) const;
+	bool IsTileSolid(Tile tile) const;
+	bool IsTileLadderTop(Tile tile) const;
+	bool IsTileLadder(Tile tile) const;
+	bool CollisionX(const Point& p, int distance) const;
+	bool CollisionY(const Point& p, int distance) const;
+	int GetLadderCenterPos(int pixel_x, int pixel_y) const;
+
+	//Tile map
+	Tile *map;
+
+	//Size of the tile map
+	int size, width, height;
+	
+	//Dictionary of tile frames
+	std::unordered_map<int, Rectangle> dict_rect;
+	
+	//Tile sheet
+	const Texture2D *img_tiles;
+};
+
